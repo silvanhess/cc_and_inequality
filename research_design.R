4bf741a1898b300c138a8f08ecdb4170f3b3d7e3
Silvan Hess <silvan.hess@stud.unilu.ch>
2025-01-11 19:20:50 +0100

    fixed spatial join

--- research_design.R
+++ research_design.R
@@ -1,26 +1,35 @@
 # Load Libraries --------------------------------------------------------------------------------------------------
 
 library(tidyverse)
-library(readxl)
-library(haven)
-library(labelled)
-library(countrycode)
+# library(readxl)
+# library(haven)
+# library(labelled)
+# library(countrycode)
 library(tmap)
 library(sf) # für shape files
 
 
 # Import data -----------------------------------------------------------------------------------------------------
 
-# deeznuts
+# geographical data
+# download from https://ec.europa.eu/eurostat/web/gisco/geodata/statistical-units/territorial-units-statistics
+# NUTS 2010
 
 sf_nuts <- read_sf("data/NUTS/NUTS_RG_20M_2010_3035.shp/NUTS_RG_20M_2010_3035.shp")
 
+# sf_nuts |> 
+#   group_by(CNTR_CODE) |>
+#   summarize(count = n()) |>
+#   view()
+
 # survey data
+# Download from https://ess.sikt.no/en/
 
 ESS10 <- read_csv("data/ESS/ESS10/ESS10.csv")
 ESS8 <- read_csv("data/ESS/ESS8e02_3/ESS8e02_3.csv")
 
 # flood data
+# download from https://essd.copernicus.org/articles/16/5145/2024/essd-16-5145-2024-assets.html
 
 HANZE <- read_csv("data/HANZE/v2.1/HANZE_events.csv")
 
@@ -33,7 +42,9 @@ nuts <-
   sf_nuts |> 
   as_tibble() |> 
   rename(region = NUTS_ID) |> 
-  select(region,LEVL_CODE, NAME_LATN, geometry)
+  select(region, CNTR_CODE, LEVL_CODE, NAME_LATN, geometry)
+
+
 
 # prepare a dataset with survey participants from wave eight and ten
 
@@ -59,8 +70,14 @@ ESS_prepared <-
     impenv <= 6
   ) |> 
   mutate(
-    gndr = factor(gndr, levels = c(1, 2), labels = c("Male", "Female"))
+    gndr = factor(gndr, levels = c(1, 2), labels = c("Male", "Female")),
+    cntry = if_else(cntry == "GB", "UK", cntry) # change GB to UK in cntry
   )
+
+# ESS_prepared |> 
+#   group_by(cntry) |> 
+#   summarize(count = n()) |> 
+#   view()
 
 # ESS_prepared |> 
 #   filter(essround == 8) |>
@@ -104,6 +121,7 @@ HANZE_prepared <-
 #   summarise(count = n())
 
 # Spatial Merge from level 3 to level 2 ---------------------------------------------------------------------------------
+
 # countries that only have level 2 regions in the ESS dataset
 level_2_countries <- c("AT", "BE", "CH", "ES", "FR", "GR", "IT", "NL", "NO", "PL", "PT") 
 
@@ -116,25 +134,41 @@ HANZE_level2_old <-
 # Filter nuts dataset to include only Level 2 regions
 nuts_level2 <- 
   nuts %>%
-  filter(LEVL_CODE == 2) |> 
+  filter(LEVL_CODE == 2,
+         CNTR_CODE %in% level_2_countries) |> 
   st_as_sf()
 
 # Perform spatial join to check if the events are within the Level 2 regions
-containment_check <- st_within(HANZE_level2_old, nuts_level2)
+containment_check <- st_intersects(HANZE_level2_old, nuts_level2)
+# containment_check <- st_within(HANZE_level2_old, nuts_level2)
 
 # Join the small polygons with the large ones based on containment
-joined <- st_join(HANZE_level2_old, nuts_level2, join = st_within)
+level2_joined <- st_join(HANZE_level2_old, nuts_level2, join = st_intersects)
 
 # select the columns of the level 2 regions
 HANZE_level2_new <- 
-  joined |> 
-    select(ID, end_date, country, region.y, LEVL_CODE.y, NAME_LATN.y, geometry) |>
-    rename(region = region.y, LEVL_CODE = LEVL_CODE.y, NAME_LATN = NAME_LATN.y)
+  level2_joined |> 
+  select(ID, end_date, country, region.y, LEVL_CODE.y, NAME_LATN.y) |>
+  as_tibble() |>  
+  select(-geometry) |> 
+  rename(region = region.y, LEVL_CODE = LEVL_CODE.y, NAME_LATN = NAME_LATN.y) |> 
+  distinct(ID, region, .keep_all = TRUE) |> # delete duplicates
+  left_join(y = nuts) |> # add the polygons of level 2 regions
+  select(-CNTR_CODE)
+
+
   
+# visualize on map
+map <- tm_basemap("OpenStreetMap") +
+  tm_shape(HANZE_level2_new$geometry) +
+  tm_polygons(col = "black")
+# tmap_mode("view")
+# print(map)
+
 
 # Spatial Merge from level 3 to level 1 --------------------------------------------------------------------------------
 
-level_1_countries <- c("DE", "GB", "ME") # countries that only have level 1 regions in the ESS dataset
+level_1_countries <- c("DE", "UK", "ME") # countries that only have level 1 regions in the ESS dataset
 
 # Filter HANZE_events_prepared_geom for countries that have Level 2 regions
 HANZE_level1_old <- 
@@ -145,20 +179,35 @@ HANZE_level1_old <-
 # Filter nuts dataset to include only Level 1 regions
 nuts_level1 <- 
   nuts %>%
-  filter(LEVL_CODE == 1) |> 
+  filter(LEVL_CODE == 1,
+         CNTR_CODE %in% level_1_countries) |> 
   st_as_sf()
 
 # Perform spatial join to check if the events are within the Level 1 regions
-containment_check <- st_within(HANZE_level1_old, nuts_level1)
+containment_check <- st_intersects(HANZE_level1_old, nuts_level1)
 
 # Join the small polygons with the large ones based on containment
-joined <- st_join(HANZE_level1_old, nuts_level1, join = st_within)
+level1_joined <- st_join(HANZE_level1_old, nuts_level1, join = st_intersects)
 
 # select the columns of the level 1 regions
 HANZE_level1_new <- 
-  joined |> 
-  select(ID, end_date, country, region.y, LEVL_CODE.y, NAME_LATN.y, geometry) |>
-  rename(region = region.y, LEVL_CODE = LEVL_CODE.y, NAME_LATN = NAME_LATN.y)
+  level1_joined |> 
+  select(ID, end_date, country, region.y, LEVL_CODE.y, NAME_LATN.y) |>
+  as_tibble() |>  
+  select(-geometry) |> # remove the polygons of level 3 regions
+  rename(region = region.y, LEVL_CODE = LEVL_CODE.y, NAME_LATN = NAME_LATN.y) |> 
+  distinct(ID, region, .keep_all = TRUE) |> # delete duplicates
+  left_join(y = nuts) |> # add the polygons of level 1 regions
+  select(-CNTR_CODE)
+
+# visualize on map
+map <- tm_basemap("OpenStreetMap") +
+  tm_shape(HANZE_level2_new$geometry) +
+  tm_polygons(col = "black") +
+  tm_shape(HANZE_level1_new$geometry) +
+  tm_polygons(col = "red")
+# tmap_mode("view")
+# print(map)
 
 
 # Put the three Datasets together ---------------------------------------------------------------------------------
@@ -170,10 +219,17 @@ HANZE_level3 <-
 
 HANZE_combined <- 
   bind_rows(HANZE_level3, HANZE_level2_new, HANZE_level1_new) |> 
-  arrange(ID) |> 
-  distinct(across(-geometry), .keep_all = TRUE) # remove duplicates
+  mutate(
+    LEVL_CODE = if_else(is.na(LEVL_CODE), 2, LEVL_CODE)
+  )
 
-HANZE_combined
+# visualize on map
+map <- tm_basemap("OpenStreetMap") +
+  tm_shape(HANZE_combined$geometry) +
+  tm_polygons(col = "black")
+#tmap_mode("view")
+#print(map)
+
 
 # Create the Treatment and Control Groups -------------------------------------------------------------------------
 
@@ -182,16 +238,56 @@ HANZE_combined
 
 treatment_group <-     
   left_join(x = HANZE_combined, 
-            y = ESS_prepared
+            y = ESS_prepared,
+            relationship = "many-to-many"
   ) |> 
   group_by(across(-region)) |> 
   summarize(
     region = paste(unique(region), collapse = ";"),
-    .groups = "drop")
+    .groups = "drop") |> 
+  select(
+    idno, essround, date, cntry, region, LEVL_CODE, NAME_LATN, agea, gndr, lrscale, impenv, ID, end_date, geometry
+  ) # arrange the variables manually
 
 
-treatment_group_before <- treatment_group |> filter(essround == 8)
-treatment_group_after <- treatment_group |> filter(essround == 10) 
+treatment_group |> 
+  group_by(region) |> 
+  summarize(count = n()) |> 
+  view() # some regions only have 1 Person -> drop them
+
+region_selector <- 
+  treatment_group |> 
+    group_by(region) |> 
+    summarize(count = n()) |> 
+    filter(count >= 30) |>
+    select(region) |> 
+    as.list()
+
+
+# create dataframe with regions that have at least 30 obvervations
+treatment_group_filtered <- 
+  treatment_group |> 
+    filter(region %in% region_selector$region)
+
+
+treatment_group_filtered |> 
+  group_by(region) |> 
+  summarize(count = n()) |> 
+  view() # now we have regions with at least 30 observations
+
+
+# visualize on a map
+treatment_group_geom <- treatment_group_filtered |>
+  group_by(region, LEVL_CODE, geometry) |>
+  summarise(count = n())
+map <- tm_basemap("OpenStreetMap") +
+  tm_shape(treatment_group_geom$geometry) +
+  tm_polygons(col = "black")
+tmap_mode("view")
+print(map)
+
+treatment_group_before <- treatment_group_filtered |> filter(essround == 8)
+treatment_group_after <- treatment_group_filtered |> filter(essround == 10) 
 
 
 # identify the surveyed people that haven't experienced floods
@@ -199,7 +295,7 @@ treatment_group_after <- treatment_group |> filter(essround == 10)
 
 
 control_group <- ESS_prepared %>%
-  filter(!idno %in% treatment_group$idno)
+  filter(!idno %in% treatment_group_filtered$idno)
 
 control_group_before <- control_group |> filter(essround == 8)
 control_group_after <- control_group |> filter(essround == 10) 
@@ -220,15 +316,6 @@ survey_respondents_per_group <-
     dimnames = list(c("Treatment Group", "Control Group"), c("Before", "After", "Total")),
     byrow = TRUE
   )
-
-treatment_group$ID |> n_distinct() # 96 Floods in the treatment group
-treatment_group$region |> n_distinct() # 136 affected regions in the treatment group
-
-treatment_group |> 
-  group_by(ID) |> 
-  summarize(count = n()) |> 
-  view() # many Floods only have 1 Person -> drop them
-
 
 # Plot the events over time ---------------------------------------------------------------------------------------
 
@@ -260,15 +347,21 @@ ESS_prepared_geom_2 <-
   group_by(region, cntry, LEVL_CODE, geometry) |> 
   summarise(count = n()) |> 
   filter(
-    LEVL_CODE == 2,
-    cntry %in% level_2_countries)
+    LEVL_CODE == 2)
 
 ESS_prepared_geom_1 <- 
   ESS_prepared |> 
   group_by(region, cntry, LEVL_CODE, geometry) |> 
   summarise(count = n()) |> 
-  filter(cntry %in% level_1_countries)
+  filter(
+    LEVL_CODE == 1)
 
+ESS_geom <- 
+  ESS_prepared |> 
+  group_by(region, LEVL_CODE, geometry) |> 
+  summarize(number_of_participants_per_region = n(), .groups = "drop") |> 
+  filter(
+    LEVL_CODE >= 1)
 
 HANZE_geom <- 
   left_join(x = HANZE_combined, y = nuts) |> 
@@ -276,111 +369,30 @@ HANZE_geom <-
   group_by(region, LEVL_CODE, geometry) |> 
   summarise(count = n())
 
-treatment_group_geom <- treatment_group |> 
-  group_by(region, LEVL_CODE, geometry) |> 
+treatment_group_geom <- treatment_group_filtered |>
+  group_by(region, LEVL_CODE, geometry) |>
   summarise(count = n())
 
-control_group_geom <- control_group |> 
-  group_by(region, LEVL_CODE, geometry) |> 
-  summarise(count = n())
+control_group_geom <- control_group |>
+  group_by(region, LEVL_CODE, geometry) |>
+  summarise(count = n()) |> 
+  drop_na(LEVL_CODE)
 
 map <- tm_basemap("OpenStreetMap") +
-  tm_shape(ESS_prepared_geom_3$geometry) +
-  tm_polygons(col = "red") +
-  tm_shape(ESS_prepared_geom_2$geometry) +
-  tm_polygons(col = "green") +
-  tm_shape(ESS_prepared_geom_1$geometry) +
-  tm_polygons(col = "purple") +
+  # tm_shape(ESS_prepared_geom_1$geometry) +
+  # tm_polygons(col = "white") +
+  # tm_shape(ESS_prepared_geom_2$geometry) +
+  # tm_polygons(col = "white") +
+  # tm_shape(ESS_prepared_geom_3$geometry) +
+  # tm_polygons(col = "white") +
+  # tm_shape(ESS_geom$geometry) +
+  # tm_polygons(col = "red") +
   tm_shape(HANZE_geom$geometry) +
-  tm_polygons(col = "blue")
-  # tm_shape(treatment_group_geom$geometry) +
-  # tm_polygons(col = "yellow") +
-  # tm_shape(control_group$geometry) +
-  # tm_polygons(col = "black")
+  tm_polygons(col = "blue") +
+  tm_shape(treatment_group_geom$geometry) +
+  tm_polygons(col = "yellow") +
+  tm_shape(control_group_geom$geometry) +
+  tm_polygons(col = "black")
 tmap_mode("view")
 tmap_options(check.and.fix = TRUE)
 print(map)
-
-
-
-
-
-# HANZE old version -----------------------------------------------------------------------------------------------
-
-HANZE_v1 <- read_excel("data/HANZE/V1.0/Events_floods.xlsx")
-
-HANZE_v1_prepared <-
-  HANZE_v1 |>
-  rename(
-    end_date = `End date`,
-    country_name = `Country name`
-  ) |>
-  mutate(
-    "ISO3" = countrycode(country_name, origin = 'country.name', destination = 'iso3c')
-  ) |>
-  filter(
-    ISO3 %in% c(ESS10_prepared_countries$ISO3),
-    between(Year, 2010, 2014)
-  )
-
-
-# NUTS -----------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-sf_nuts_prepared <-
-  sf_nuts |>
-  filter(LEVL_CODE == "2",
-         NUTS_ID %in% ESS10_prepared_grouped$region
-  )
-
-
-
-
-# FloodArchive ----------------------------------------------------------------------------------------------------
-
-
-FloodArchive <- read_excel("data/FloodArchive.xlsx")
-
-FloodArchive_prepared <-
-  FloodArchive |>
-  filter(
-    as.Date(Ended, format = "%Y-%m-%d") >= as.Date("2017-01-01")
-  ) |>
-  mutate(
-    ISO3_main = countrycode(Country, origin = 'country.name', destination = 'iso3c'),
-    ISO3_other = countrycode(OtherCountry, origin = 'country.name', destination = 'iso3c')
-  ) |>
-  filter(
-    ISO3_main %in% c(ESS10_prepared_countries$ISO3) | ISO3_other %in% c(ESS10_prepared_countries$ISO3)
-  )
-
-sf_FloodArchive_prepared <- st_as_sf(FloodArchive_prepared, coords = c("long", "lat"), crs = 4326)
-map_FloodArchive <- tm_basemap("OpenStreetMap") +
-  tm_shape(sf_FloodArchive_prepared) +
-  tm_dots(col = "blue", size = 0.1)
-tmap_mode("view")
-print(map_FloodArchive)
-
-
-
-
-# emdat -----------------------------------------------------------------------------------------------------------
-
-
-
-emdat <- read_excel("data/emdat.xlsx")
-
-
-emdat_prepared <-
-  emdat |> filter(
-    ISO %in% c(ESS10_prepared_countries$ISO3),
-    `End Year` >= 2016 & `End Year` <= 2020,
-    `Disaster Type` == "Flood"
-  )
-
-
